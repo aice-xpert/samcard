440dfd50011a2ddc57d4ca09c31f041610bd2053
codderrrrr <saadwaqar211@gmail.com>
2026-02-26 15:46:31 +0500

    add CTA Banner section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { CTABanner } from "@/components/CTABanner";
 import Footer from "@/components/Footer";
 import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return <>
     <Testimonials/>
+    <CTABanner/>
     <Footer/>
   </>
 }
--- components/TestimonialsCard.tsx
+++ components/TestimonialsCard.tsx
@@ -2,36 +2,36 @@ import { Testimonial } from "@/constant";
 import { Star } from "lucide-react";
 
 interface TestimonialCardProps {
-  testimonial: Testimonial;
+    testimonial: Testimonial;
 }
 
 export default function TestimonialsCard({
-  testimonial,
+    testimonial,
 }: TestimonialCardProps) {
-  return (
-    <div className="bg-white/5 backdrop-blur-sm border border-white/10 rounded-2xl p-8 hover:border-[#008001]/100 transition-all">
-      {/* Rating */}
-      <div className="flex gap-1 mb-4">
-        {[...Array(testimonial.rating)].map((_, i) => (
-          <Star key={i} className="text-[#49B618] fill-[#49B618]" size={20} />
-        ))}
-      </div>
+    return (
+        <div className="bg-white/5 backdrop-blur-sm border border-white/10 rounded-2xl p-8 hover:border-[#008001]/100 transition-all">
+            {/* Rating */}
+            <div className="flex gap-1 mb-4">
+                {[...Array(testimonial.rating)].map((_, i) => (
+                    <Star key={i} className="text-[#49B618] fill-[#49B618]" size={20} />
+                ))}
+            </div>
 
-      <p className="text-gray-300 leading-relaxed mb-6">"{testimonial.text}"</p>
+            <p className="text-gray-300 leading-relaxed mb-6">"{testimonial.text}"</p>
 
-      <div className="flex items-center gap-3">
-        <img
-          src={testimonial.avatar}
-          alt={testimonial.name}
-          className="w-12 h-12 rounded-full object-cover"
-        />
-        <div>
-          <div className="font-semibold text-white">{testimonial.name}</div>
-          <div className="text-sm text-gray-400">
-            {testimonial.role} at {testimonial.company}
-          </div>
+            <div className="flex items-center gap-3">
+                <img
+                    src={testimonial.avatar}
+                    alt={testimonial.name}
+                    className="w-12 h-12 rounded-full object-cover"
+                />
+                <div>
+                    <div className="font-semibold text-white">{testimonial.name}</div>
+                    <div className="text-sm text-gray-400">
+                        {testimonial.role} at {testimonial.company}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
