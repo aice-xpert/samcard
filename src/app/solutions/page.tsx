c6beb1005007a1ff25e132f2ac17822bd8a07d9a
codderrrrr <saadwaqar211@gmail.com>
2026-03-02 15:21:07 +0500

    Change them of Solutions page

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,11 +1,13 @@
 import {CTABanner} from "@/components/CTABanner";
 import {Hero} from "@/components/solutions/Hero-Solutions";
 import { HighlightSection } from "@/components/solutions/HighlightSection";
+import SolutionsGrid from "@/components/solutions/SolutionGrid";
 export default function SolutionsPage() {
   return (
     <>
       
       <Hero />
+      <SolutionsGrid/>
       <HighlightSection />
       <CTABanner />
       
