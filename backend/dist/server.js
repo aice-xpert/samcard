1fccb638c272768056039c3d5b7b03e96814620d
Zainab <zainab19604khalil@gmail.com>
2026-03-26 22:24:58 +0500

    added erd flow diagram and md for the schema

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -3,18 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const signup_1 = __importDefault(require("./auth/signup"));
 const login_1 = __importDefault(require("./auth/login"));
-// Load environment variables from parent directory
-dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../.env') });
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+console.log(path_1.default.resolve(__dirname, "../../.env"));
+dotenv_1.default.config({
+    path: path_1.default.resolve(__dirname, "../../.env"),
+});
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
 app.get('/', (req, res) => {
     res.send('Backend server is running!');
 });
