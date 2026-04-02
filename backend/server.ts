35321768a466b2449c733616afc6d44e21c33624
Zainab <zainab19604khalil@gmail.com>
2026-04-02 16:42:28 +0500

    server.ts - fix : cors issue - backend deployment

--- backend/server.ts
+++ backend/server.ts
@@ -29,7 +29,8 @@ const app = express();
 const PORT: number | string = process.env.PORT || 5001;
 
 const allowedOrigins = [
-  process.env.FRONTEND_URL || "http://localhost:3000",
+  "http://samcard.vercel.app",
+"http://localhost:3000",
   "http://localhost:3000",
   "http://localhost:3001",
 ];
