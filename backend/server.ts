22a0c695f12eea335114ffe0da18030205d81feb
Zainab <zainab19604khalil@gmail.com>
2026-04-22 21:14:16 +0500

    fix : cors issue before testing live

--- backend/server.ts
+++ backend/server.ts
@@ -36,6 +36,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://samcard-6auvfjong-zk19604s-projects.vercel.app",
+  "https://samcard-git-fix-zainab-live-bugs-zk19604s-projects.vercel.app",
 ];
 
 const corsOptions: cors.CorsOptions = {
